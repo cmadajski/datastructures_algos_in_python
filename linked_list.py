5eedc30efbea36ba4f4d3464dc756b2e3841eacb
Christian Madajski <cmad7317@tutanota.com>
2022-03-16 15:28:53 -0400

    define Node class with constructor

---
+++ linked_list.py
@@ -0,0 +1,15 @@
+# define a Node class
+class Node():
+    # create constructor for Node instantiation
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
